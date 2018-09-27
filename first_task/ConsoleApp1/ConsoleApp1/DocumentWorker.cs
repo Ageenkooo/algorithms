9c8181635cdf5b0b725428678a125b58a4f74cd1
Irina Ageenko <ira.ageenkoo@gmail.com>
2018-09-28 01:07:43 +0300

    added first task

---
+++ first_task/ConsoleApp1/ConsoleApp1/DocumentWorker.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class DocumentWorker
+    {
+        public int[] RandomArrCreator()
+        {
+            Random rnd = new Random();
+            int[] arr = new int[100000];
+            for (int i = 0; i < 100000; i++)
+            {
+                int value = rnd.Next(0, 100000);
+                arr[i] = value;
+            }
+            return arr;
+        }
+    }
+}
---
+++ first_task/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DocumentWorker dw = new DocumentWorker();
+            for(int i=0; i<50; i++)
+            {
+                File.WriteAllText($@"D:\c#\ConsoleApp1\numbers{i}.txt", string.Join(" ", dw.RandomArrCreator()));
+            }
+
+            string output = " i - HS QS \r\n";
+            for (int i=7; i<200; i++)
+            {
+                int resH = 0;
+                int resQ = 0;
+                for (int j = 0; j < 50; j++)
+                {
+                    int[] arr1 = File.ReadAllText($@"D:\c#\ConsoleApp1\numbers{j}.txt").Split(' ').Select(y => int.Parse(y)).ToArray();
+                    int[] arr2 = File.ReadAllText($@"D:\c#\ConsoleApp1\numbers{j}.txt").Split(' ').Select(y => int.Parse(y)).ToArray();
+
+                    Sorter sort = new Sorter();
+
+                    Stopwatch stopwatch = Stopwatch.StartNew();
+                    sort.HybridQuicksort(arr1, 0, arr1.Length - 1, i);
+                    stopwatch.Stop();
+
+                    Stopwatch stopwatch2 = Stopwatch.StartNew();
+                    sort.Quicksort(arr2, 0, arr2.Length - 1);
+                    stopwatch2.Stop();
+
+                    resH += (int)stopwatch.ElapsedMilliseconds;
+                    resQ += (int)stopwatch2.ElapsedMilliseconds;
+                }
+                resH /= 50;
+                resQ /= 50;
+                if (resH <= resQ)
+                {
+                    output = output + " " + i + " - " + resH + " " + resQ + " \r\n";
+                }
+            }
+            File.WriteAllText(@"D:\c#\ConsoleApp1\sortedNumbers.txt", output);
+        }
+    }
+}
