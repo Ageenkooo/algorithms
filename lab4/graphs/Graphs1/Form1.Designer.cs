c84b19e5eaa7d39256000e3c18940aa50b575084
Irina Ageenko <ira.ageenkooo@gmail.com>
2018-11-02 20:55:50 +0000

    added 4th lab

---
+++ lab4/graphs/Graphs1/Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace Graphs1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBoxMatrix = new System.Windows.Forms.ListBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.selectButton = new System.Windows.Forms.Button();
+            this.buttonInc = new System.Windows.Forms.Button();
+            this.buttonAdj = new System.Windows.Forms.Button();
+            this.deleteALLButton = new System.Windows.Forms.Button();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.drawEdgeButton = new System.Windows.Forms.Button();
+            this.drawVertexButton = new System.Windows.Forms.Button();
+            this.sheet = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.sheet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBoxMatrix
+            // 
+            this.listBoxMatrix.FormattingEnabled = true;
+            this.listBoxMatrix.Location = new System.Drawing.Point(733, 123);
+            this.listBoxMatrix.Name = "listBoxMatrix";
+            this.listBoxMatrix.Size = new System.Drawing.Size(217, 303);
+            this.listBoxMatrix.TabIndex = 6;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(969, 24);
+            this.menuStrip1.TabIndex = 12;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // selectButton
+            // 
+            this.selectButton.Image = global::Graphs1.Properties.Resources.cursor;
+            this.selectButton.Location = new System.Drawing.Point(12, 12);
+            this.selectButton.Name = "selectButton";
+            this.selectButton.Size = new System.Drawing.Size(45, 45);
+            this.selectButton.TabIndex = 9;
+            this.selectButton.UseVisualStyleBackColor = true;
+            this.selectButton.Click += new System.EventHandler(this.selectButton_Click);
+            // 
+            // buttonInc
+            // 
+            this.buttonInc.Location = new System.Drawing.Point(858, 39);
+            this.buttonInc.Name = "buttonInc";
+            this.buttonInc.Size = new System.Drawing.Size(92, 52);
+            this.buttonInc.TabIndex = 8;
+            this.buttonInc.Text = "Матрица инциндентности";
+            this.buttonInc.UseVisualStyleBackColor = true;
+            this.buttonInc.Click += new System.EventHandler(this.buttonInc_Click);
+            // 
+            // buttonAdj
+            // 
+            this.buttonAdj.Location = new System.Drawing.Point(733, 39);
+            this.buttonAdj.Name = "buttonAdj";
+            this.buttonAdj.Size = new System.Drawing.Size(92, 52);
+            this.buttonAdj.TabIndex = 7;
+            this.buttonAdj.Text = "Матрица смежности";
+            this.buttonAdj.UseVisualStyleBackColor = true;
+            this.buttonAdj.Click += new System.EventHandler(this.buttonAdj_Click);
+            // 
+            // deleteALLButton
+            // 
+            this.deleteALLButton.Image = global::Graphs1.Properties.Resources.deleteAll;
+            this.deleteALLButton.Location = new System.Drawing.Point(12, 276);
+            this.deleteALLButton.Name = "deleteALLButton";
+            this.deleteALLButton.Size = new System.Drawing.Size(45, 45);
+            this.deleteALLButton.TabIndex = 5;
+            this.deleteALLButton.UseVisualStyleBackColor = true;
+            this.deleteALLButton.Click += new System.EventHandler(this.deleteALLButton_Click);
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Image = global::Graphs1.Properties.Resources.delete;
+            this.deleteButton.Location = new System.Drawing.Point(12, 225);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(45, 45);
+            this.deleteButton.TabIndex = 3;
+            this.deleteButton.UseVisualStyleBackColor = true;
+            this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);
+            // 
+            // drawEdgeButton
+            // 
+            this.drawEdgeButton.Image = global::Graphs1.Properties.Resources.edge;
+            this.drawEdgeButton.Location = new System.Drawing.Point(12, 115);
+            this.drawEdgeButton.Name = "drawEdgeButton";
+            this.drawEdgeButton.Size = new System.Drawing.Size(45, 45);
+            this.drawEdgeButton.TabIndex = 2;
+            this.drawEdgeButton.UseVisualStyleBackColor = true;
+            this.drawEdgeButton.Click += new System.EventHandler(this.drawEdgeButton_Click);
+            // 
+            // drawVertexButton
+            // 
+            this.drawVertexButton.Image = global::Graphs1.Properties.Resources.vertex;
+            this.drawVertexButton.Location = new System.Drawing.Point(13, 64);
+            this.drawVertexButton.Name = "drawVertexButton";
+            this.drawVertexButton.Size = new System.Drawing.Size(45, 45);
+            this.drawVertexButton.TabIndex = 1;
+            this.drawVertexButton.UseVisualStyleBackColor = true;
+            this.drawVertexButton.Click += new System.EventHandler(this.drawVertexButton_Click);
+            // 
+            // sheet
+            // 
+            this.sheet.BackColor = System.Drawing.SystemColors.Control;
+            this.sheet.Location = new System.Drawing.Point(114, 12);
+            this.sheet.Name = "sheet";
+            this.sheet.Size = new System.Drawing.Size(590, 415);
+            this.sheet.TabIndex = 0;
+            this.sheet.TabStop = false;
+            this.sheet.MouseClick += new System.Windows.Forms.MouseEventHandler(this.sheet_MouseClick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 199);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(46, 20);
+            this.textBox1.TabIndex = 14;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 163);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 13);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "введите вес ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 175);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 13);
+            this.label2.TabIndex = 16;
+            this.label2.Text = "нажмите enter";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(719, 94);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(127, 13);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "с эйлером и областями";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(969, 444);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.selectButton);
+            this.Controls.Add(this.buttonInc);
+            this.Controls.Add(this.buttonAdj);
+            this.Controls.Add(this.listBoxMatrix);
+            this.Controls.Add(this.deleteALLButton);
+            this.Controls.Add(this.deleteButton);
+            this.Controls.Add(this.drawEdgeButton);
+            this.Controls.Add(this.drawVertexButton);
+            this.Controls.Add(this.sheet);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "vscode.ru";
+            ((System.ComponentModel.ISupportInitialize)(this.sheet)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox sheet;
+        private System.Windows.Forms.Button drawVertexButton;
+        private System.Windows.Forms.Button drawEdgeButton;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.Button deleteALLButton;
+        private System.Windows.Forms.ListBox listBoxMatrix;
+        private System.Windows.Forms.Button buttonAdj;
+        private System.Windows.Forms.Button buttonInc;
+        private System.Windows.Forms.Button selectButton;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ lab4/graphs/Graphs1/Form1.cs
@@ -0,0 +1,442 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Graphs1
+{
+    public partial class Form1 : Form
+    {
+        DrawGraph G;
+        List<Vertex> V;
+        List<Edge> E;
+        List<String> Value;
+        List<Weight> W;
+        List<int> Euler;
+        int[,] AMatrix; //матрица смежности
+        int[,] IMatrix; //матрица инцидентности
+        int[,] e;
+        int selected1; //выбранные вершины, для соединения линиями
+        int selected2;
+
+        public Form1()
+        {
+            InitializeComponent();
+            int initialValue = 1;
+            V = new List<Vertex>();
+            G = new DrawGraph(sheet.Width, sheet.Height);
+            E = new List<Edge>();
+            W = new List<Weight>();
+            Value = new List<String>();
+            Value.Add(initialValue.ToString());
+            Euler = new List<int>();
+            
+            sheet.Image = G.GetBitmap();
+        }
+
+        //кнопка - выбрать вершину
+        private void selectButton_Click(object sender, EventArgs e)
+        {
+            selectButton.Enabled = false;
+            drawVertexButton.Enabled = true;
+            drawEdgeButton.Enabled = true;
+            deleteButton.Enabled = true;
+            G.clearSheet();
+            G.drawALLGraph(V, E, W);
+            sheet.Image = G.GetBitmap();
+            selected1 = -1;
+        }
+
+        //кнопка - рисовать вершину
+        private void drawVertexButton_Click(object sender, EventArgs e)
+        {
+            drawVertexButton.Enabled = false;
+            selectButton.Enabled = true;
+            drawEdgeButton.Enabled = true;
+            deleteButton.Enabled = true;
+            G.clearSheet();
+            G.drawALLGraph(V, E, W);
+            sheet.Image = G.GetBitmap();
+        }
+
+        //кнопка - рисовать ребро
+        private void drawEdgeButton_Click(object sender, EventArgs e)
+        {
+            drawEdgeButton.Enabled = false;
+            selectButton.Enabled = true;
+            drawVertexButton.Enabled = true;
+            deleteButton.Enabled = true;
+            G.clearSheet();
+            G.drawALLGraph(V, E, W);
+            sheet.Image = G.GetBitmap();
+            selected1 = -1;
+            selected2 = -1;
+        }
+
+        //кнопка - удалить элемент
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+            deleteButton.Enabled = false;
+            selectButton.Enabled = true;
+            drawVertexButton.Enabled = true;
+            drawEdgeButton.Enabled = true;
+            G.clearSheet();
+            G.drawALLGraph(V, E, W);
+            sheet.Image = G.GetBitmap();
+        }
+
+        //кнопка - удалить граф
+        private void deleteALLButton_Click(object sender, EventArgs e)
+        {
+            selectButton.Enabled = true;
+            drawVertexButton.Enabled = true;
+            drawEdgeButton.Enabled = true;
+            deleteButton.Enabled = true;
+            const string message = "Вы действительно хотите полностью удалить граф?";
+            const string caption = "Удаление";
+            var MBSave = MessageBox.Show(message, caption, MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (MBSave == DialogResult.Yes)
+            {
+                V.Clear();
+                E.Clear();
+                G.clearSheet();
+                sheet.Image = G.GetBitmap();
+            }
+        }
+
+        //кнопка - матрица смежности
+        private void buttonAdj_Click(object sender, EventArgs e)
+        {
+            createAdjAndOut();
+        }
+
+        //кнопка - матрица инцидентности 
+        private void buttonInc_Click(object sender, EventArgs e)
+        {
+            createIncAndOut();
+        }
+
+        private void sheet_MouseClick(object sender, MouseEventArgs e)
+        {
+            //нажата кнопка "выбрать вершину", ищем степень вершины
+            if (selectButton.Enabled == false)
+            {
+                for (int i = 0; i < V.Count; i++)
+                {
+                    if (Math.Pow((V[i].x - e.X), 2) + Math.Pow((V[i].y - e.Y), 2) <= G.R * G.R)
+                    {
+                        if (selected1 != -1)
+                        {
+                            selected1 = -1;
+                            G.clearSheet();
+                            G.drawALLGraph(V, E, W);
+                            sheet.Image = G.GetBitmap();
+                        }
+                        if (selected1 == -1)
+                        {
+                            G.drawSelectedVertex(V[i].x, V[i].y);
+                            selected1 = i;
+                            sheet.Image = G.GetBitmap();
+                            createAdjAndOut();
+                            listBoxMatrix.Items.Clear();
+                            int degree = 0;
+                            for (int j = 0; j < V.Count; j++)
+                                degree += AMatrix[selected1, j];
+                            listBoxMatrix.Items.Add("Степень вершины №" + (selected1 + 1) + " равна " + degree);
+                            break;
+                        }
+                    }
+                }
+            }
+            //нажата кнопка "рисовать вершину"
+            if (drawVertexButton.Enabled == false)
+            {
+                V.Add(new Vertex(e.X, e.Y));
+                G.drawVertex(e.X, e.Y, V.Count.ToString());
+                sheet.Image = G.GetBitmap();
+            }
+            //нажата кнопка "рисовать ребро"
+            if (drawEdgeButton.Enabled == false)
+            {
+                if (e.Button == MouseButtons.Left)
+                {
+                    for (int i = 0; i < V.Count; i++)
+                    {
+                        if (Math.Pow((V[i].x - e.X), 2) + Math.Pow((V[i].y - e.Y), 2) <= G.R * G.R)
+                        {
+                            if (selected1 == -1)
+                            {
+                                G.drawSelectedVertex(V[i].x, V[i].y);
+                                selected1 = i;
+                                sheet.Image = G.GetBitmap();
+                                break;
+                            }
+                            if (selected2 == -1)
+                            {
+                                G.drawSelectedVertex(V[i].x, V[i].y);
+                                selected2 = i;
+                                E.Add(new Edge(selected1, selected2));
+                                W.Add(new Weight(selected1, selected2, Value[Value.Count-1]));
+                                G.drawEdge(V[selected1], V[selected2], E[E.Count - 1], E.Count - 1, W[W.Count - 1] );
+                                selected1 = -1;
+                                selected2 = -1;
+                                sheet.Image = G.GetBitmap();
+                                break;
+                            }
+                        }
+                    }
+                }
+                if (e.Button == MouseButtons.Right)
+                {
+                    if ((selected1 != -1) &&
+                        (Math.Pow((V[selected1].x - e.X), 2) + Math.Pow((V[selected1].y - e.Y), 2) <= G.R * G.R))
+                    {
+                        G.drawVertex(V[selected1].x, V[selected1].y, (selected1 + 1).ToString());
+                        selected1 = -1;
+                        sheet.Image = G.GetBitmap();
+                    }
+                }
+            }
+            //нажата кнопка "удалить элемент"
+            if (deleteButton.Enabled == false)
+            {
+                bool flag = false; //удалили ли что-нибудь по ЭТОМУ клику
+                //ищем, возможно была нажата вершина
+                for (int i = 0; i < V.Count; i++)
+                {
+                    if (Math.Pow((V[i].x - e.X), 2) + Math.Pow((V[i].y - e.Y), 2) <= G.R * G.R)
+                    {
+                        for (int j = 0; j < E.Count; j++)
+                        {
+                            if ((E[j].v1 == i) || (E[j].v2 == i))
+                            {
+                                E.RemoveAt(j);
+                                j--;
+                            }
+                            else
+                            {
+                                if (E[j].v1 > i) E[j].v1--;
+                                if (E[j].v2 > i) E[j].v2--;
+                            }
+                        }
+                        V.RemoveAt(i);
+                        flag = true;
+                        break;
+                    }
+                }
+                //ищем, возможно было нажато ребро
+                if (!flag)
+                {
+                    for (int i = 0; i < E.Count; i++)
+                    {
+                        if (E[i].v1 == E[i].v2) //если это петля
+                        {
+                            if ((Math.Pow((V[E[i].v1].x - G.R - e.X), 2) + Math.Pow((V[E[i].v1].y - G.R - e.Y), 2) <= ((G.R + 2) * (G.R + 2))) &&
+                                (Math.Pow((V[E[i].v1].x - G.R - e.X), 2) + Math.Pow((V[E[i].v1].y - G.R - e.Y), 2) >= ((G.R - 2) * (G.R - 2))))
+                            {
+                                E.RemoveAt(i);
+                                flag = true;
+                                break;
+                            }
+                        }
+                        else //не петля
+                        {
+                            if (((e.X - V[E[i].v1].x) * (V[E[i].v2].y - V[E[i].v1].y) / (V[E[i].v2].x - V[E[i].v1].x) + V[E[i].v1].y) <= (e.Y + 4) &&
+                                ((e.X - V[E[i].v1].x) * (V[E[i].v2].y - V[E[i].v1].y) / (V[E[i].v2].x - V[E[i].v1].x) + V[E[i].v1].y) >= (e.Y - 4))
+                            {
+                                if ((V[E[i].v1].x <= V[E[i].v2].x && V[E[i].v1].x <= e.X && e.X <= V[E[i].v2].x) ||
+                                    (V[E[i].v1].x >= V[E[i].v2].x && V[E[i].v1].x >= e.X && e.X >= V[E[i].v2].x))
+                                {
+                                    E.RemoveAt(i);
+                                    flag = true;
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+                //если что-то было удалено, то обновляем граф на экране
+                if (flag)
+                {
+                    G.clearSheet();
+                    G.drawALLGraph(V, E, W);
+                    sheet.Image = G.GetBitmap();
+                }
+            }
+        }
+
+        //создание матрицы смежности и вывод в листбокс
+        private void createAdjAndOut()
+        {
+            AMatrix = new int[V.Count, V.Count];
+            G.fillAdjacencyMatrix(V.Count, E, AMatrix, W);
+            listBoxMatrix.Items.Clear();
+            string sOut = "    ";
+            for (int i = 0; i < V.Count; i++)
+                sOut += (i + 1) + " ";
+            listBoxMatrix.Items.Add(sOut);
+            for (int i = 0; i < V.Count; i++)
+            {
+                sOut = (i + 1) + " | ";
+                for (int j = 0; j < V.Count; j++)
+                    sOut += AMatrix[i, j] + " ";
+                listBoxMatrix.Items.Add(sOut);
+            }
+            int k = 0;
+            bool isEuler = true;
+            for (int i = 0; i < V.Count; i++)
+            {
+                for (int j = 0; j < V.Count; j++)
+                {
+                    if(AMatrix[i,j] != 0)
+                    {
+                        k++;
+                    }
+                }
+                if (k%2 != 0 || k == 0)
+                {
+                    isEuler = false;
+                    break;
+                }
+                k = 0;
+            }
+            listBoxMatrix.Items.Add(isEuler.ToString());
+            string[] used = new string[V.Count];
+            string symbol = "area";
+            int m = 0;
+
+            for (int i = 0; i<V.Count; i++)
+            {
+
+                if (String.IsNullOrEmpty(used[i]))
+                {
+                    bfs(i, AMatrix, used, symbol);
+                    m++;
+                    symbol = (string)("area" + m);
+                }
+            }
+            G.clearSheet();
+            G.drawALLGraphWithAreas(V, E, W, used);
+            sheet.Image = G.GetBitmap();
+
+             if (isEuler)
+             {
+                 e = new int[V.Count, V.Count];
+                 euler(0, AMatrix);
+             }
+        }
+
+        private void euler(int i, int[,] AMatrix)
+        {
+            if (Euler.Count == 0)
+            {
+                Euler.Add(0);
+            }
+            for (int j = 0; j < V.Count; j++) 
+            {
+                if (AMatrix[i, j] != 0 && e[i, j] == 0)
+                {
+                    
+                    if ((j == 0) && (Euler.Count == V.Count))
+                    {
+                        var str = string.Empty;
+                        Euler.ForEach(el => { str += (el + 1) + " "; });
+                        Euler = new List<int>();
+                        listBoxMatrix.Items.Add("Cikl:\n" + str);
+
+                    }
+                    else
+                    {
+                        Euler.Add(j);
+                        e[i, j] = 1;
+                        e[j, i] = 1;
+                        euler(j, AMatrix);
+                    }
+
+                }
+            }
+        }
+
+        //создание матрицы инцидентности и вывод в листбокс
+        private void createIncAndOut()
+        {
+            if (E.Count > 0)
+            {
+                IMatrix = new int[V.Count, E.Count];
+                G.fillIncidenceMatrix(V.Count, E, IMatrix);
+                listBoxMatrix.Items.Clear();
+                string sOut = "    ";
+                for (int i = 0; i < E.Count; i++)
+                    sOut += (char)('a' + i) + " ";
+                listBoxMatrix.Items.Add(sOut);
+                for (int i = 0; i < V.Count; i++)
+                {
+                    sOut = (i + 1) + " | ";
+                    for (int j = 0; j < E.Count; j++)
+                        sOut += IMatrix[i, j] + " ";
+                    listBoxMatrix.Items.Add(sOut);
+                }
+            }
+            else
+                listBoxMatrix.Items.Clear();
+        }
+
+        private void bfs(int v, int[,] AMatrix, string[] used, string symbol)
+        {
+            used[v] = symbol;
+            Queue<int> q = new Queue<int>();
+            q.Enqueue(v);
+            while (q.Count != 0)
+            {
+                v = q.Dequeue();
+                for (int i=0; i< V.Count; i++)
+                {
+                    if(AMatrix[i, v] != 0 &&  String.IsNullOrEmpty(used[i]))
+                    {
+                        used[i] = symbol;
+                        q.Enqueue(i);
+                    }
+                }
+            }
+        }
+        
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            if (sheet.Image != null)
+            {
+                SaveFileDialog savedialog = new SaveFileDialog();
+                savedialog.Title = "Сохранить картинку как...";
+                savedialog.OverwritePrompt = true;
+                savedialog.CheckPathExists = true;
+                savedialog.Filter = "Image Files(*.BMP)|*.BMP|Image Files(*.JPG)|*.JPG|Image Files(*.GIF)|*.GIF|Image Files(*.PNG)|*.PNG|All files (*.*)|*.*";
+                savedialog.ShowHelp = true;
+                if (savedialog.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        sheet.Image.Save(savedialog.FileName, System.Drawing.Imaging.ImageFormat.Jpeg);
+                    }
+                    catch
+                    {
+                        MessageBox.Show("Невозможно сохранить изображение", "Ошибка",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                this.Value.Add(textBox1.Text);
+            }
+        }
+    }
+}
